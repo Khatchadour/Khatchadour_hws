a01e080d5a1fa2d3f19c7bd5083d179c035ea2b4
Khatchatur <khatchadourpapikian@gmail.com>
2018-09-29 03:37:42 +0400

    Google, my love. Deadline: October 4th

---
+++ Shahane/Khatchadour_Papikian_homework2/exercise_3.py
@@ -0,0 +1,10 @@
+a = [[0 for i in range(4)] for j in range(4)]
+print(a)
+for i in range(4):
+    for j in range(4):
+        if a[i]==a[j]:
+            a[i][j]=1
+        else: 
+            a[i][j]=2
+    print(a[i])
+        
